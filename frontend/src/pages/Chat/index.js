dfc180f53f2500b92bc279385be754cc3f362293
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2020-05-30 16:15:38 -0300

    caixas de msg

--- frontend/src/pages/Chat/index.js
+++ frontend/src/pages/Chat/index.js
@@ -9,10 +9,37 @@ export default function Chat() {
     <div className="chat-container">
       <div className="content">
         <div className="users">
-
         </div>
 
         <div className="principal">
+
+          <div className="chat">
+            <div className="list">
+              <div className="msg-box-mine">
+                <div className="user-img"></div>
+                <div className="flr">
+                  <div className="message">
+                    <div className="msg">
+                      <p>alalalasahshshshsshshshshshs</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="msg-box-other">
+                <div className="user-img"></div>
+                <div className="flr">
+                  <div className="message">
+                    <div className="msg"> <p>alalala
+                    s
+                    sssssssssssssssss
+                    s
+                      ssaina ais aa aj</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div id="divisao">
             <div id="texto">
             </div>
