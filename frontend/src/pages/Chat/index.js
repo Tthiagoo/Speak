8dd4161268a17eecf5405ecfcfa7b748877ebef0
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2020-05-30 16:23:52 -0300

    estilização front msg

--- frontend/src/pages/Chat/index.js
+++ frontend/src/pages/Chat/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import io from 'socket.io-client';
 import './styles.css'
 
 export default function Chat() {
   //const socket = io('http://localhost:3333')
+  const [msg, stateMsg] = useState('');
+  const [mensagens, stateMensagens] = useState([]);
+
+  function HandleSubmit(event) {
+    stateMsg(event.target.value)
+  }
+  function HandleForm(event) {
+    event.preventDefault()
+    if (msg.trim()) {
+      stateMsg('')
+    }
+  }
+
 
   return (
     <div className="chat-container">
@@ -12,37 +25,42 @@ export default function Chat() {
         </div>
 
         <div className="principal">
-
           <div className="chat">
             <div className="list">
-              <div className="msg-box-mine">
+              <div className="msg-box-other">
                 <div className="user-img"></div>
                 <div className="flr">
                   <div className="message">
-                    <div className="msg">
+                    <div className="msg-mine">
                       <p>alalalasahshshshsshshshshshs</p>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="msg-box-other">
+
+              <div className="msg-box-mine">
                 <div className="user-img"></div>
                 <div className="flr">
                   <div className="message">
-                    <div className="msg"> <p>alalala
-                    s
-                    sssssssssssssssss
-                    s
-                      ssaina ais aa aj</p>
+                    <div className="msg-mine">
+                      <p>alalalasahshshshsshshshshshs</p>
                     </div>
                   </div>
                 </div>
               </div>
+              
             </div>
           </div>
           <div id="divisao">
-            <div id="texto">
-            </div>
+            <form onSubmit={HandleForm}>
+              <input id="texto" placeholder="mensagem"
+                onChange={HandleSubmit}
+                style={{ width: 1000 }}
+                type="text"
+                value={msg}
+              />
+            </form>
+
           </div>
         </div>
 
