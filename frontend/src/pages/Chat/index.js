cbf6b0eae07adfbec9a605e132590c8bbc20d2e7
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2020-11-30 10:43:16 -0300

    Update index.js

--- frontend/src/pages/Chat/index.js
+++ frontend/src/pages/Chat/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState} from 'react';
-import {useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import queryString from 'query-string';
 import io from 'socket.io-client'
@@ -25,19 +25,19 @@ export default function Chat({ location }) {
   const [messages, setMessages] = useState([])
   const [name, setName] = useState('')
   const [foto_url, setFoto] = useState('')
-  
+
 
   const history = useHistory()
-  
-  const ENDPOINT = 'https://speak-server.herokuapp.com/'
-   
+
+  const ENDPOINT = 'ws://https://speak-server.herokuapp.com/'
+
 
   useEffect(() => {
     const { username, room } = queryString.parse(location.search)
     setUserName(username)
     setRoom(room)
-    
-    socket = io(ENDPOINT)
+
+    socket = io(ENDPOINT, { transports: ['websocket'] })
 
     setName(localStorage.getItem('name'))
     const localName = (localStorage.getItem('name'))
@@ -46,34 +46,34 @@ export default function Chat({ location }) {
 
     setFoto(localStorage.getItem('foto_url'))
     const localFoto = (localStorage.getItem('foto_url'))
-    
-    
 
-    socket.emit('join',{ username, room, foto_url:localFoto, name:localName, bio:localBio}, (error) => { 
-      if(error) {
+
+
+    socket.emit('join', { username, room, foto_url: localFoto, name: localName, bio: localBio }, (error) => {
+      if (error) {
         alert(error);
         socket.emit('disconnect')
         socket.off()
         history.push('/')
-      } 
-      
+      }
+
     })
 
   }, [ENDPOINT, location.search])
-  
+
   useEffect(() => {
-    
+
     socket.on('message', message => {
       setMessages(messages => [...messages, message]);
     });
 
     socket.on("roomData", ({ users }) => {
-      
+
       setUsers(users);
       console.log(users)
-      
+
     });
-    
+
   }, []);
   const sendMessage = (event) => {
     event.preventDefault()
@@ -83,24 +83,24 @@ export default function Chat({ location }) {
   }
   return (
     <Fade in={true} timeout={1000}>
-    <div className="chat-container">
-      <div className="content">
-        <div className="users">
-          <Perfil foto={foto_url} name={name} />
-          <FriendsList />
-          <ConectedList users={users}/>
-        </div>
-        <div className="principal">
-          <div className="chat">
-            <Tab room={room} />
-            <Messages messages={messages} username={username} />
+      <div className="chat-container">
+        <div className="content">
+          <div className="users">
+            <Perfil foto={foto_url} name={name} />
+            <FriendsList />
+            <ConectedList users={users} />
           </div>
-          <Input message={message} sendMessage={sendMessage} setMessage={setMessage} />
+          <div className="principal">
+            <div className="chat">
+              <Tab room={room} />
+              <Messages messages={messages} username={username} />
+            </div>
+            <Input message={message} sendMessage={sendMessage} setMessage={setMessage} />
+
+          </div>
 
         </div>
-
       </div>
-    </div>
     </Fade>
   )
 }
